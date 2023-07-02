01553c229493f41a086cdd33713b97f5a3ad3c53
benny <benny@benny-optiplex-7060>
2023-07-02 11:55:43 +0100

    updated

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -2,10 +2,18 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\File;
+
 class ImageService
 {
     public function uploadImage($file, $path): string
     {
+        $fullPath = storage_path('app/public/' . $path);
+
+        if (!File::exists($fullPath)) {
+            File::makeDirectory($fullPath, 0755, true);
+        }
+
         $filename = time() . '_' . $file->getClientOriginalName();
         $file->storeAs('public/'.$path, $filename);
         return asset('storage/'.$path.'/'.$filename);

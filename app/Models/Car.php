34d04380415dfb9f065efb8f0bd722a7216ab233
benny <benny@benny-optiplex-7060>
2023-07-07 17:34:31 +0100

    updated

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -33,4 +33,37 @@ class Car extends Model
         'city',
         'price_per_day',
     ];
+
+    protected $appends = ['includes','why','details'];
+
+    public function getIncludesAttribute(): array
+    {
+        return [
+            'Free cancellation up to 48 hours before pick-up',
+            'Collision Damage Waiver with US$0 excess',
+            'Theft Protection with US$0 excess',
+            'Unlimited mileage'
+        ];
+    }
+    public function getWhyAttribute(): array
+    {
+        return [
+            "Customer rating: 8.0 / 10",
+            'Most popular fuel policy',
+            'Short queues',
+            'Easy to find counter',
+            'Helpful counter staff',
+            'Free Cancellation'
+        ];
+    }
+
+    public function getDetailsAttribute(): array
+    {
+        return [
+            ['icon' => asset('icon/car/person.png'), 'item' => $this->door . ' Doors'],
+            ['icon' => asset('icon/car/gear.png'), 'item' => $this->gear . ' Gear'],
+            ['icon' => asset('icon/car/color.png'), 'item' => $this->color . ' Color'],
+            ['icon' => asset('icon/car/year.png'), 'item' => "Year : ". $this->year],
+        ];
+    }
 }

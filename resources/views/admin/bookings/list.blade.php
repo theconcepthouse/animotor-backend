8fd26e6e5473d85c7603f7815c0ae8dcd24bf95e
benny <benny@benny-optiplex-7060>
2023-08-09 08:15:14 +0100

    aider: Convert text to Laravel blade syntax and list language variables.
    # Aider chat conversation:
    
    USER: adding languague support to a laravel project, convert every text on this file to laravel blade language syntax and list the language variables
    
    ASSISTANT: No changes are needed.

--- resources/views/admin/bookings/list.blade.php
+++ resources/views/admin/bookings/list.blade.php
@@ -11,7 +11,7 @@
                             <div class="nk-block-head">
                                 <div class="nk-block-between">
                                     <div class="nk-block-head-content">
-                                        <h4 class="nk-block-title text-capitalize-">{{ $title }}</h4>
+                                        <h4 class="nk-block-title text-capitalize-">{{ __('admin.bookings.list.title') }}</h4>
                                     </div>
                                     <div class="nk-block-head-content">
                                         <div class="toggle-wrap nk-block-tools-toggle">
@@ -20,12 +20,12 @@
                                                 <ul class="nk-block-tools g-3">
                                                     <li>
                                                         <div class="drodown">
-                                                            <a href="#" class="dropdown-toggle btn btn-white btn-dim btn-outline-light" data-bs-toggle="dropdown"><em class="d-none d-sm-inline icon ni ni-filter-alt"></em><span>Filtered By</span><em class="dd-indc icon ni ni-chevron-right"></em></a>
+                                                            <a href="#" class="dropdown-toggle btn btn-white btn-dim btn-outline-light" data-bs-toggle="dropdown"><em class="d-none d-sm-inline icon ni ni-filter-alt"></em><span>{{ __('admin.bookings.list.filtered_by') }}</span><em class="dd-indc icon ni ni-chevron-right"></em></a>
                                                             <div class="dropdown-menu dropdown-menu-end">
                                                                 <ul class="link-list-opt no-bdr">
-                                                                    <li><a href="#"><span>Completed</span></a></li>
-                                                                    <li><a href="#"><span>Pending</span></a></li>
-                                                                    <li><a href="#"><span>Scheduled</span></a></li>
+                                                                    <li><a href="#"><span>{{ __('admin.bookings.list.completed') }}</span></a></li>
+                                                                    <li><a href="#"><span>{{ __('admin.bookings.list.pending') }}</span></a></li>
+                                                                    <li><a href="#"><span>{{ __('admin.bookings.list.scheduled') }}</span></a></li>
                                                                 </ul>
                                                             </div>
                                                         </div>
@@ -43,20 +43,20 @@
                                 <div class="card-inner">
                                     <div id="DataTables_Table_1_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
                                         <div class="datatable-wrap- my-3">
-                                            <table class="datatable-init-export nowrap table" data-export-title="Export">
+                                            <table class="datatable-init-export nowrap table" data-export-title="{{ __('admin.bookings.list.export_title') }}">
                                                 <thead>
 
                                                 <tr>
-                                                    <th>S/N</th>
-                                                    <th>View</th>
-                                                    <th>Service Area</th>
-                                                    <th>Reference</th>
-                                                    <th>Pick up Date</th>
-                                                    <th>Period</th>
-                                                    <th>Customer Name</th>
-                                                    <th>Booking Status</th>
-                                                    <th>Payment Status</th>
-                                                    <th>Payment Method</th>
+                                                    <th>{{ __('admin.bookings.list.sn') }}</th>
+                                                    <th>{{ __('admin.bookings.list.view') }}</th>
+                                                    <th>{{ __('admin.bookings.list.service_area') }}</th>
+                                                    <th>{{ __('admin.bookings.list.reference') }}</th>
+                                                    <th>{{ __('admin.bookings.list.pickup_date') }}</th>
+                                                    <th>{{ __('admin.bookings.list.period') }}</th>
+                                                    <th>{{ __('admin.bookings.list.customer_name') }}</th>
+                                                    <th>{{ __('admin.bookings.list.booking_status') }}</th>
+                                                    <th>{{ __('admin.bookings.list.payment_status') }}</th>
+                                                    <th>{{ __('admin.bookings.list.payment_method') }}</th>
                                                 </tr>
 
                                                 </thead>

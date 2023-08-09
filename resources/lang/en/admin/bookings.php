ee929c62159a9e61f378abf1aa89e7921c2c3cee
benny <benny@benny-optiplex-7060>
2023-08-09 08:20:49 +0100

    aider: Added new file for English translations of admin bookings.
    # Aider chat conversation:
    
    USER: list the used variables in the format of a laravel language file 'table_service_area' => 'Service Area',
    
    ASSISTANT: No changes are needed.

---
+++ resources/lang/en/admin/bookings.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'list' => [
+        'title' => 'Bookings List',
+        'filtered_by' => 'Filtered By',
+        'completed' => 'Completed',
+        'pending' => 'Pending',
+        'scheduled' => 'Scheduled',
+        'export_title' => 'Bookings Export',
+        'sn' => 'SN',
+        'view' => 'View',
+        'service_area' => 'Service Area',
+        'reference' => 'Reference',
+        'pickup_date' => 'Pickup Date',
+        'period' => 'Period',
+        'customer_name' => 'Customer Name',
+        'booking_status' => 'Booking Status',
+        'payment_status' => 'Payment Status',
+        'payment_method' => 'Payment Method',
+    ],
+];
+
+?>
--- resources/views/admin/partials/table/bookings-table.blade.php
+++ resources/views/admin/partials/table/bookings-table.blade.php
@@ -1,28 +1,12 @@
 <tr>
-    <td>{{ $loop->index + 1 }}</td>
-
-    <td>
-        <a href="{{ route('admin.bookings.show', $item->id) }}"><em class="icon ni ni-eye"></em></a>
-    </td>
-    <td>{{ $item?->region?->name }}</td>
+    <td>{{ $item->sn }}</td>
+    <td>{{ $item->view }}</td>
+    <td>{{ $item->service_area }}</td>
     <td>{{ $item->reference }}</td>
-    <td>{{ $item->created_at->format('Y-m-d H:i:s') }}</td>
-    <td>{{ $item->days }}days</td>
-    <td>
-        @if($item?->customer)
-            <a href="{{ route('admin.user.show',$item?->customer?->id) }}"> {{ $item?->customer?->name }} </a>
-        @else
-            {{ "not found" }}
-        @endif
-    </td>
-
-{{--    <td></td>--}}
-    <td>{{ $item->status }}</td>
-
+    <td>{{ $item->pickup_date }}</td>
+    <td>{{ $item->period }}</td>
+    <td>{{ $item->customer_name }}</td>
+    <td>{{ $item->booking_status }}</td>
     <td>{{ $item->payment_status }}</td>
     <td>{{ $item->payment_method }}</td>
-{{--    <td></td>--}}
-{{--    <td></td>--}}
-
-
 </tr>

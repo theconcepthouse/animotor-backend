c5245a2ee3dcba411e75671f4b5eb079b657c383
Ichie Benjamin <benny@Ichies-MacBook-Pro.local>
2023-09-18 20:32:36 +0100

    updated

--- resources/views/frontpage/components/contact_form.blade.php
+++ resources/views/frontpage/components/contact_form.blade.php
@@ -1,39 +1,44 @@
 <section class="cars__ticket pb-120 pb__10">
     <div class="container">
         <div class="row align-items-center justify-content-center">
-            <div class="col-md-4">
-                <div class="input__grp">
-                    <label for="first_name">First Name </label>
-                    <div class="form-group mt-2">
-                        <input required class="form-control" value="" name="first_name" type="text" id="first_name" placeholder="First Name">
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="input__grp">
+                            <label for="first_name">First Name </label>
+                            <div class="form-group mt-2">
+                                <input required class="form-control" value="" name="first_name" type="text" id="first_name" placeholder="First Name">
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="input__grp">
-                    <label for="email">Email Address </label>
-                    <div class="form-group mt-2">
-                        <input required class="form-control" value="" name="email" type="text" id="email" placeholder="Email Address">
+                    <div class="col-md-4">
+                        <div class="input__grp">
+                            <label for="email">Email Address </label>
+                            <div class="form-group mt-2">
+                                <input required class="form-control" value="" name="email" type="text" id="email" placeholder="Email Address">
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
 
-            <div class="col-md-4">
-                <div class="input__grp">
-                    <label for="last_name">Last Name </label>
-                    <div class="form-group mt-2">
-                        <input required class="form-control" value="" name="last_name" type="text" id="email" placeholder="Last Name">
+                    <div class="col-md-4">
+                        <div class="input__grp">
+                            <label for="last_name">Last Name </label>
+                            <div class="form-group mt-2">
+                                <input required class="form-control" value="" name="last_name" type="text" id="email" placeholder="Last Name">
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
 
 
-            <div class="col-md-12">
-                <div class="input__grp">
-                    <label for="actions_taken">Your message</label>
-                    <div class="form-group mt-2">
-                        <textarea rows="6" required class="form-control" name="message" type="text" id="message" placeholder="message"></textarea>
+                    <div class="col-md-12 mt-5">
+                        <div class="input__grp">
+                            <label for="actions_taken">Your message</label>
+                            <div class="form-group mt-2">
+                                <textarea rows="6" required class="form-control" name="message" type="text" id="message" placeholder="message"></textarea>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
             </div>
 

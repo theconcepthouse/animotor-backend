6f832bccaf1705ce26b947ef16c66860c6dc43d9
benny <benjaminchukwudi0@gmail.com>
2023-10-06 09:32:42 +0100

    updated

--- app/Services/RegionService.php
+++ app/Services/RegionService.php
@@ -9,30 +9,18 @@ class RegionService
 {
     public function getRegionByLatLng($lat, $lng){
 
-        $zones = Region::whereContains('coordinates', new Point($lat, $lng, env('POINT_SRID',0)))->latest()->get(['id', 'name','parent_id','is_active']);
-//        if(count($zones) < 1)
-//        {
-//            return
-//        }
-//        $data = array_filter($zones->toArray(), function($zone){
-//            if($zone['status'] == 1) {
-//                return $zone;
-//            }
-//        });
-//
-//        if (count($data) > 0) {
-//            return response()->json(['zone_id' => json_encode(array_column($data, 'id')), 'zone_data'=>array_values($data)], 200);
-//        }
-//        return Region::first();
-
         $firstRegionWithParentId = Region::whereContains('coordinates', new Point($lat, $lng, env('POINT_SRID', 0)))
             ->whereNotNull('parent_id')
+            ->where('is_active', true)
             ->latest()
+            ->get(['id', 'name','parent_id','is_active'])
             ->first();
 
         if (!$firstRegionWithParentId) {
             $firstRegionWithParentId = Region::whereContains('coordinates', new Point($lat, $lng, env('POINT_SRID', 0)))
+                ->where('is_active', true)
                 ->latest()
+                ->get(['id', 'name','parent_id','is_active'])
                 ->first();
         }
 

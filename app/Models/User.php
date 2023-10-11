f1b85dd537f9b3b08eeb786a2b9cfd7dc3f04909
benny <benjaminchukwudi0@gmail.com>
2023-10-11 16:27:47 +0100

    updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable implements LaratrustUser, Wallet
 
     protected $casts = [
         'is_online' => 'bool',
-        'last_location_update' => 'date',
+        'last_location_update' => 'datetime',
     ];
 
     protected $hidden = [

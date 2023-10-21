3dfae8182f927b9dae23ce092032b8f0a7f08d35
benny <benjaminchukwudi0@gmail.com>
2023-10-21 19:58:16 +0100

    updd

--- app/Services/Firebase/FirestoreService.php
+++ app/Services/Firebase/FirestoreService.php
@@ -20,86 +20,90 @@ class FirestoreService
 
     public function updateTripRequest($trip_data, $drivers_id = null, $drivers = null):void
     {
-        $firestoreClient = $this->firestoreClient;
+        try {
+            $firestoreClient = $this->firestoreClient;
 
-        $data = [
-            'region_id' => $trip_data->region_id,
-            'driver_id' => $trip_data->driver_id,
-            'id' => $trip_data->id,
-            'status' => $trip_data->status,
-            'customer_id' => $trip_data->customer_id,
-            'fee' => $trip_data->fee,
-            'origin' => $trip_data->origin,
-            'destination' => $trip_data->destination,
-            'payment_status' => $trip_data->payment_status,
-            'payment_method' => $trip_data->payment_method,
-            'origin_lat' => floatval($trip_data->origin_lat),
-            'origin_lng' => floatval($trip_data->origin_lng),
-            'destination_lat' => floatval($trip_data->destination_lat),
-            'destination_lng' => floatval($trip_data->destination_lng),
-            'started_at' => $trip_data->started_at_val,
-            'end_at' => $trip_data->end_at_val,
-            'current_lat' => $trip_data->current_lat,
-            'current_lng' => $trip_data->current_lng,
-            'distance' => $trip_data->distance,
-            'distance_text' => $trip_data->distance_text,
-            'duration' => $trip_data->duration,
-            'duration_text' => $trip_data->duration_text,
-            'car_id' => $trip_data->car_id,
-            'completed' => $trip_data->completed,
-            'cancelled' => $trip_data->cancelled,
-            'rating' => $trip_data->rating,
-            'driver_rating' => $trip_data->driver_rating,
-            'rating_comment' => $trip_data->rating_comment,
-            'driver_rating_comment' => $trip_data->driver_rating_comment,
-            'driver_feedback' => $trip_data->driver_feedback,
-            'rider_feedback' => $trip_data->rider_feedback,
-            'base_price' => $trip_data->base_price,
-            'time_price' => $trip_data->time_price,
-            'distance_price' => $trip_data->distance_price,
-            'discount' => $trip_data->discount,
-            'tax' => $trip_data->tax,
-            'grand_total' => $trip_data->grand_total,
-            'service_id' => $trip_data->service_id,
-            'ride_type' => $trip_data->ride_type,
+            $data = [
+                'region_id' => $trip_data->region_id,
+                'driver_id' => $trip_data->driver_id,
+                'id' => $trip_data->id,
+                'status' => $trip_data->status,
+                'customer_id' => $trip_data->customer_id,
+                'fee' => $trip_data->fee,
+                'origin' => $trip_data->origin,
+                'destination' => $trip_data->destination,
+                'payment_status' => $trip_data->payment_status,
+                'payment_method' => $trip_data->payment_method,
+                'origin_lat' => floatval($trip_data->origin_lat),
+                'origin_lng' => floatval($trip_data->origin_lng),
+                'destination_lat' => floatval($trip_data->destination_lat),
+                'destination_lng' => floatval($trip_data->destination_lng),
+                'started_at' => $trip_data->started_at_val,
+                'end_at' => $trip_data->end_at_val,
+                'current_lat' => $trip_data->current_lat,
+                'current_lng' => $trip_data->current_lng,
+                'distance' => $trip_data->distance,
+                'distance_text' => $trip_data->distance_text,
+                'duration' => $trip_data->duration,
+                'duration_text' => $trip_data->duration_text,
+                'car_id' => $trip_data->car_id,
+                'completed' => $trip_data->completed,
+                'cancelled' => $trip_data->cancelled,
+                'rating' => $trip_data->rating,
+                'driver_rating' => $trip_data->driver_rating,
+                'rating_comment' => $trip_data->rating_comment,
+                'driver_rating_comment' => $trip_data->driver_rating_comment,
+                'driver_feedback' => $trip_data->driver_feedback,
+                'rider_feedback' => $trip_data->rider_feedback,
+                'base_price' => $trip_data->base_price,
+                'time_price' => $trip_data->time_price,
+                'distance_price' => $trip_data->distance_price,
+                'discount' => $trip_data->discount,
+                'tax' => $trip_data->tax,
+                'grand_total' => $trip_data->grand_total,
+                'service_id' => $trip_data->service_id,
+                'ride_type' => $trip_data->ride_type,
 
-            'currency' => $trip_data->currency,
-            'drivers' => $drivers_id ?? [],
+                'currency' => $trip_data->currency,
+                'drivers' => $drivers_id ?? [],
 
-            'customer_avatar' => $trip_data?->customer?->avatar,
-            'customer_phone' => $trip_data?->customer?->full_phone,
-        ];
-
-        if($trip_data->driver){
-            $extra = [
-                'driver_avatar' => $trip_data?->driver?->avatar,
-                'driver_name' => $trip_data?->driver?->name,
-                'driver_phone' => $trip_data?->driver?->full_phone,
-                'car_title' => $trip_data?->driver?->car?->title,
-                'car_vehicle_no' => $trip_data?->driver?->car?->vehicle_no,
-                'car_year' => $trip_data?->driver?->car?->year,
-                'car_model' => $trip_data?->driver?->car?->model,
-                'car_type' => $trip_data?->service?->name,
-                'car_color' => $trip_data?->driver?->car?->color,
+                'customer_avatar' => $trip_data?->customer?->avatar,
+                'customer_phone' => $trip_data?->customer?->full_phone,
             ];
 
-
-            $data = array_merge($data, $extra);
-        }else{
-            if($drivers && count($drivers) > 0){
-                $driver = $drivers->first();
+            if($trip_data->driver){
                 $extra = [
-                    'driver_avatar' => $driver?->avatar,
-                    'driver_name' => $driver?->name,
-                    'message' => "The driver will be on their way as soon as they confirm"
+                    'driver_avatar' => $trip_data?->driver?->avatar,
+                    'driver_name' => $trip_data?->driver?->name,
+                    'driver_phone' => $trip_data?->driver?->full_phone,
+                    'car_title' => $trip_data?->driver?->car?->title,
+                    'car_vehicle_no' => $trip_data?->driver?->car?->vehicle_no,
+                    'car_year' => $trip_data?->driver?->car?->year,
+                    'car_model' => $trip_data?->driver?->car?->model,
+                    'car_type' => $trip_data?->service?->name,
+                    'car_color' => $trip_data?->driver?->car?->color,
                 ];
 
-                $data = array_merge($data, $extra);
-            }
-        }
 
-        if(!env('APP_DEBUG')) {
-           $firestoreClient->updateDocument("rideRequests/" . $trip_data->id, $data);
+                $data = array_merge($data, $extra);
+            }else{
+                if($drivers && count($drivers) > 0){
+                    $driver = $drivers->first();
+                    $extra = [
+                        'driver_avatar' => $driver?->avatar,
+                        'driver_name' => $driver?->name,
+                        'message' => "The driver will be on their way as soon as they confirm"
+                    ];
+
+                    $data = array_merge($data, $extra);
+                }
+            }
+
+            if(!env('APP_DEBUG')) {
+                $firestoreClient->updateDocument("rideRequests/" . $trip_data->id, $data);
+            }
+        }catch (\Exception $e) {
+            info('fire error : '. $e->getMessage());
         }
 
     }

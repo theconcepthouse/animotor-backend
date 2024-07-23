9421bc4d2802730986cf364a8493904bcd156a55
Gnosis <emmagab38@gmail.com>
2024-07-23 22:18:41 +0100

    fixing migration

--- database/migrations/2024_07_01_082636_add_user_id_to_rates.php
+++ database/migrations/2024_07_01_082636_add_user_id_to_rates.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('rates', function (Blueprint $table) {
-            $table->bigInteger('driver_id');
+            $table->uuid('driver_id');
 
             $table->foreign('driver_id')->references('id')->on('users')
                 ->onDelete('cascade')
